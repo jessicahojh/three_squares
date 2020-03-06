7f37d529784153c3728f876aea97f099979d6d6f
Jessica <jessica.ho.jh@gmail.com>
2020-03-06 12:08:20 -0800

    three squares with alert

---
+++ three_square.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    $("#div1").click(function(){
+    alert("Div 1 was clicked.");
+    });
+
+    $("#div2").click(function(){
+    alert("Div 2 was clicked.");
+    });
+
+    $("#div3").click(function(){
+    alert("Div 3 was clicked.");
+    });
+});
