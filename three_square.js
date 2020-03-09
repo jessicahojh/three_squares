f0ae6cbce5f63ec873675fea961973475e260abe
Jessica <jessica.ho.jh@gmail.com>
2020-03-09 14:39:23 -0700

    three squares complete

--- three_square.js
+++ three_square.js
@@ -1,14 +1,8 @@
-$(document).ready(function(){
-    
-    $("#div1").click(function(){
-    alert("Div 1 was clicked.");
-    });
-
-    $("#div2").click(function(){
-    alert("Div 2 was clicked.");
-    });
-
-    $("#div3").click(function(){
-    alert("Div 3 was clicked.");
-    });
-});
+document.addEventListener("click", e => {
+    if (e.target.className == "box") {
+        alert(e.target.firstElementChild.innerText);
+    }
+    if (e.target.className == "name") {
+        alert(e.target.innerText);
+    }
+}) 
